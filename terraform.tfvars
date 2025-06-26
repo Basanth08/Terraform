b57e2fb53424ee4560f63b14912014ecc34a05b8
Basanth08 <vbasanthkumaroffl@gmail.com>
2025-06-25 21:28:15 -0400

    Initial Commits

--- terraform.tfvars
+++ terraform.tfvars
@@ -1,1 +1,9 @@
-region = ""
+region = "us-east-1"
+vpc_cidr = "192.168.0.0/16"
+all_cidr = "0.0.0.0/0"
+public_subnet1_cidr = "192.168.1.0/24"
+public_subnet2_cidr = "192.168.2.0/24"
+private_subnet_cidr = "192.168.3.0/24"
+availability_zone = "us-east-1a"
+jenkins_port = 8080
+ssh_port = 22
--- variables.tf
+++ variables.tf
@@ -1,3 +1,27 @@
 variable "region"{
     type = string
 }
+variable "vpc_cidr"{
+    type = string
+}
+variable "all_cidr"{
+    type = string
+}
+variable "public_subnet1_cidr"{
+    type = string
+}
+variable "public_subnet2_cidr"{
+    type = string
+}
+variable "private_subnet_cidr"{
+    type = string
+}
+variable "availability_zone"{
+    type = string
+}
+variable "jenkins_port"{
+    type = number
+}
+variable "ssh_port"{
+    type = number
+}
