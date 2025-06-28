f5b3ca9098ecfffa007d0592d1780055730d5ff0
Basanth08 <vbasanthkumaroffl@gmail.com>
2025-06-28 18:16:46 -0400

    commit

--- main.tf
+++ main.tf
@@ -504,3 +504,23 @@ resource "aws_autoscaling_attachment" "autoscaling-attachment" {
     autoscaling_group_name = aws_autoscaling_group.app-asg.id
     lb_target_group_arn    = aws_lb_target_group.app-target-group.arn  # Fixed: Changed from alb_target_group_arn
 }
+# Application Load Balancer
+resource "aws_lb" "app-lb" {
+    name               = "app-lb"
+    internal           = false                    # Internet-facing
+    load_balancer_type = "application"
+    security_groups    = [aws_security_group.lb_sg.id]
+    subnets           = [aws_subnet.public_subnet1.id, aws_subnet.public_subnet2.id]
+}
+
+# Load Balancer Listener
+resource "aws_lb_listener" "app-listener" {
+    load_balancer_arn = aws_lb.app-lb.arn
+    port              = "80"
+    protocol          = "HTTP"
+    
+    default_action {
+        type             = "forward"
+        target_group_arn = aws_lb_target_group.app-target-group.arn
+    }
+}
